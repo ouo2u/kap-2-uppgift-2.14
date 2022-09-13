7309d5d00319f2192d32a79fad0b9aea14642c71
ouo2u <abod98451@gmail.com>
2022-09-13 21:35:28 +0200

    Add project files.

---
+++ kap 2 uppgift 2.14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kap_2_uppgift_2._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett tal med en decmial");
+           double tal1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("skriv ett tal med en decmial till");
+          double tal2 = double.Parse(Console.ReadLine());
+
+            double produkt = tal1 * tal2;
+            Console.WriteLine("summan av tallerna är " + produkt);
+
+
+        }
+    }
+}
